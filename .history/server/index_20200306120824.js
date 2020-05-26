1742c939a3fa2a1cbf0cb4a6e513b79ff4343e96
Savannah Williams <Savi@YeeHaw.local>
2020-05-26 16:45:53 -0600

    updated port numbers

--- .history/server/index_20200306120824.js
+++ .history/server/index_20200306120824.js
@@ -8,118 +8,104 @@ const cors = require('cors');
 const db = require('./db/controllers.js');
 const url = 'https://superheroapi.com/api/10217002715925793';
 
-const port = 4000;
+const port = 5000;
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors())
 
 app.get('/:hero', (req, res, next) => {
-  const { hero } = req.params;
-  axios
-    .get(`${url}/search/${hero}`)
+  const { hero } = req.params
+  axios.get(`${url}/search/${hero}`)
     .then((data) => {
       let resultObj = {};
       let heroObj = {};
       const arr = data.data.results;
-      arr.map((obj) => {
+      arr.map(obj => {
         heroObj[obj.id] = obj.name;
         if (obj.name.toLowerCase() === hero) {
           if (resultObj.hero === undefined) {
             resultObj.hero = obj;
           }
         }
-      });
+      })
       resultObj.heros = heroObj;
       res.send(resultObj);
-    })
-    .catch((error) => {
+    }).catch(error => {
       throw error;
     });
-});
+})
 
 app.get('/index/heros', (req, res, next) => {
-  axios
-    .get(`https://akabab.github.io/superhero-api/api/all.json`)
-    .then((data) => {
-      res.send(data.data);
-    });
-});
+  axios.get(`https://akabab.github.io/superhero-api/api/all.json`)
+    .then(data => {
+      res.send(data.data)
+    })
+})
 
 app.get('/:id/image', (req, res, next) => {
   const { id } = req.params;
-  axios
-    .get(`${url}/${id}/image`)
+  axios.get(`${url}/${id}/image`)
     .then((data) => {
       res.send(data.data);
-    })
-    .catch((error) => {
+    }).catch(error => {
       throw error;
     });
-});
+})
 
 app.get('/:id/appearance', (req, res, next) => {
   const { id } = req.params;
-  axios
-    .get(`${url}/${id}/appearance`)
+  axios.get(`${url}/${id}/appearance`)
     .then((data) => {
-      res.send(data.data);
-    })
-    .catch((error) => {
+      res.send(data.data)
+    }).catch(error => {
       throw error;
     });
-});
+})
 
 app.get('/:id/biography', (req, res, next) => {
   const { id } = req.params;
-  axios
-    .get(`${url}/${id}/biography`)
+  axios.get(`${url}/${id}/biography`)
     .then((data) => {
-      res.send(data.data);
-    })
-    .catch((error) => {
+      res.send(data.data)
+    }).catch(error => {
       throw error;
     });
-});
+})
 
 app.get('/:id/connections', (req, res, next) => {
   const { id } = req.params;
-  axios
-    .get(`${url}/${id}/connections`)
+  axios.get(`${url}/${id}/connections`)
     .then((data) => {
-      res.send(data.data);
-    })
-    .catch((error) => {
+      res.send(data.data)
+    }).catch(error => {
       throw error;
     });
-});
+})
 
 app.get('/:id/powerstats', (req, res, next) => {
   const { id } = req.params;
-  axios
-    .get(`${url}/${id}/powerstats`)
+  axios.get(`${url}/${id}/powerstats`)
     .then((data) => {
-      res.send(data.data);
-    })
-    .catch((error) => {
+      res.send(data.data)
+    }).catch(error => {
       throw error;
     });
-});
+})
 
 app.get('/:id/work', (req, res, next) => {
   const { id } = req.params;
-  axios
-    .get(`${url}/${id}/work`)
+  axios.get(`${url}/${id}/work`)
     .then((data) => {
-      res.send(data.data);
-    })
-    .catch((error) => {
+      res.send(data.data)
+    }).catch(error => {
       throw error;
     });
-});
+})
 
-app.get('/team/team', db.getTeam);
+app.get('/team/team', db.getTeam)
 
-app.post('/team/:id', db.newMember);
+app.post('/team/:id', db.newMember)
 
-app.delete('/team/:id', db.deleteHero);
+app.delete('/team/:id', db.deleteHero)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
